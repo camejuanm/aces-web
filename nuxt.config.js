772a6f90fbc1dbf26c1d2919593b38b54d519f9e
Abdul Ghofar Alhasyim <abdul.ghofar@student.umn.ac.id>
2022-03-03 22:44:00 +0700

    ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  ssr: false,
+  target: 'static',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/scss/main.scss'],
