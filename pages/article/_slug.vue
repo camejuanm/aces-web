440bc08e06bfaba08ec9c7510a564db5e088ef33
unknown <abdul.ghofar@suitmedia.com>
2022-08-31 22:56:52 +0700

    update ui article

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -1,16 +1,26 @@
 <template>
   <LoadingHandler v-if="$fetchState.pending" add-class="vh-60" />
-  <main v-else class="site-main container">
-    <section class="article-header">
-      <h1 class="h1 title m-0">{{ dataArticle.title }}</h1>
-      <span class="date"
-        >Author: {{ dataArticle.author.name }} |
-        {{ $formatCurrentDate(dataArticle.updated_at) }}</span
-      >
-    </section>
-    <section class="article-content">
-      <div class="article-content-wrapper" v-html="dataArticle.body"></div>
-    </section>
+  <main v-else class="site-main">
+    <div class="article-image flex h-center">
+      <img
+        v-if="dataArticle.image != null"
+        :src="dataArticle.image"
+        class="full-width"
+        alt=""
+      />
+    </div>
+    <div class="container">
+      <section class="article-header">
+        <h1 class="h1 f-24 title m-0">{{ dataArticle.title }}</h1>
+        <span class="date"
+          >Author: {{ dataArticle.author.name }} |
+          {{ $formatCurrentDate(dataArticle.updated_at) }}</span
+        >
+      </section>
+      <section class="article-content">
+        <div class="article-content-wrapper" v-html="dataArticle.body"></div>
+      </section>
+    </div>
   </main>
 </template>
 
@@ -39,7 +49,11 @@ export default {
 <style lang="scss" scoped>
 .site-main {
   font-family: 'Poppins-Light', sans-serif;
-  padding: 1em 0;
+  padding: 0;
+
+  @media #{$large} {
+    padding-top: 2em;
+  }
 }
 
 .container {
@@ -47,20 +61,59 @@ export default {
 }
 
 .article {
+  &-image {
+    position: relative;
+    z-index: 0;
+    overflow: hidden;
+
+    width: 100%;
+    margin: 0 auto;
+    max-width: 920px;
+
+    @media #{$large} {
+      padding-left: 3em;
+    }
+
+    img {
+      max-width: 920px;
+    }
+  }
+
   &-header {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
 
-    gap: 1em;
-    padding: 1em 0;
+    background: #fff;
+    width: 100%;
 
-    border-top: 1px solid rgba($color: #000000, $alpha: 0.1);
+    position: relative;
+    z-index: 3;
+
+    gap: 1em;
+    padding: 2em 0;
+
     border-bottom: 1px solid rgba($color: #000000, $alpha: 0.1);
 
+    @media #{$md} {
+      margin-top: -3em;
+      padding-left: 1em;
+      width: 85%;
+      border: none;
+    }
+
+    @media #{$md} {
+      padding-left: 1em;
+      width: 85%;
+    }
+
+    @media #{$large} {
+      padding-left: 0;
+    }
+
     .title {
-      font-family: 'Poppins-Light', sans-serif;
-      font-weight: 300;
+      font-family: 'Poppins', sans-serif;
+      font-weight: 500;
     }
 
     .date {
@@ -71,6 +124,16 @@ export default {
   &-content {
     text-align: justify;
     overflow: hidden;
+
+    max-width: 920px;
+
+    @media #{$md} {
+      padding: 0 1em;
+    }
+
+    @media #{$large} {
+      padding: 0;
+    }
 
     &-wrapper {
       p {
