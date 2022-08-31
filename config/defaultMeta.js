bd0c1d1d77873cdae374a640858b773ebf4ba336
ghofaralhasyim <abdul.ghofar@student.umn.ac.id>
2022-08-31 22:51:14 +0700

    update meta tag

---
+++ config/defaultMeta.js
@@ -0,0 +1,83 @@
+export default [
+	{ charset: 'utf-8' },
+	{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+	{
+		hid: 'description',
+		name: 'description',
+		content: 'Association of Computer Engineering Students (ACES) merupakan himpunan mahasiswa teknik komputer Universitas Multimedia Nusantara.'
+	},
+	{
+		name: 'theme-color',
+		content: '#ffffff'
+	},
+	{
+		name: 'format-detection',
+		content: 'telephone=no'
+	},
+	{
+		hid: 'og:url',
+		property: 'og:url',
+		content: '/'
+	},
+	{
+		hid: 'og:title',
+		property: 'og:title',
+		content: 'ACES UMN'
+	},
+	{
+		hid: 'og:site_name',
+		property: 'og:site_name',
+		content: 'ACES UMN'
+	},
+	{
+		hid: 'og:description',
+		property: 'og:description',
+		content: 'Association of Computer Engineering Students (ACES) merupakan himpunan mahasiswa teknik komputer Universitas Multimedia Nusantara.'
+	},
+	{
+		hid: 'og:image',
+		property: 'og:image',
+		content: '/icon.png'
+	},
+	{
+		hid: 'og:type',
+		property: 'og:type',
+		content: 'website'
+	},
+	{
+		hid: 'fb:app_id',
+		property: 'fb:app_id',
+		content: ''
+	},
+
+	{
+		hid: 'twitter:card',
+		name: 'twitter:card',
+		content: ''
+	},
+	{
+		hid: 'twitter:site',
+		name: 'twitter:site',
+		content: '/'
+	},
+	{
+		hid: 'twitter:description',
+		name: 'twitter:description',
+		content: 'Association of Computer Engineering Students (ACES) merupakan himpunan mahasiswa teknik komputer Universitas Multimedia Nusantara.'
+	},
+	{
+		hid: 'twitter:title',
+		name: 'twitter:title',
+		content: 'ACES UMN'
+	},
+	{
+		hid: 'twitter:image',
+		name: 'twitter:image',
+		content: '/icon.png'
+	},
+	{
+		hid: 'keywords',
+		name: 'keywords',
+		content: 'ACES'
+	}
+]
