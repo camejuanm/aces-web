4d38e626ec3851ed477a96412899524848863f20
unknown <abdul.ghofar@suitmedia.com>
2022-10-14 15:56:31 +0700

    update loading handler

--- components/LoadingHandler.vue
+++ components/LoadingHandler.vue
@@ -1,9 +1,12 @@
 <template>
   <div
-    :class="addClass"
-    class="full-width flex h-center v-center text-center pt-40 pb-40 m-auto"
+    class="full-width flex flex-col h-center v-center text-center pt-40 pb-40 m-auto"
   >
     <span class="spinner"></span>
+    <span v-if="caption != null" class="mt-12 text-grey">{{ caption }}</span>
+    <span class="text-grey mt-12"
+      >[Please wait, we collecting data from server]</span
+    >
   </div>
 </template>
 
@@ -13,6 +16,10 @@ export default {
     addClass: {
       type: String,
       default: ''
+    },
+    caption: {
+      type: String,
+      default: null
     }
   }
 }
